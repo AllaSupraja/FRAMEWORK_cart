0a91d1fbf4fb5d855a868905486db974cf43867a
AllaSupraja <123451656+AllaSupraja@users.noreply.github.com>
2025-10-19 19:10:36 +0530

    Add files via upload

---
+++ test.config.ts
@@ -0,0 +1,14 @@
+export class Testconfig{
+    appurl = "https://naveenautomationlabs.com/opencart"
+    //appurl = "https://tutorialsninja.com/demo/"
+
+
+    //valid login credentials
+    email = "supraja@abc.com"
+    password = "test@123"
+
+    //product details
+    productName = "MacBook"
+    productQuantity = "2"
+    totalPrice = "$1,204.00"
+}
